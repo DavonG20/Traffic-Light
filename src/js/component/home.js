4fe05539320c5d5dddcdf8d600b93997d1bc304e
DavonG20 <Grant.Davon@live.jobcorps.org>
2021-03-31 15:04:55 +0000

    initial commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,27 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React, { useCallback, useState } from "react";
 export function Home() {
+	const [color, setColor] = useState(null);
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center-mt-5">
+			<div id="trafficlight"></div>
+			<div id="container">
+				<div
+					id="redlight"
+					className={color == "red" ? "redlight active" : "redlight"}
+					onMouseOver={e => setColor("red")}></div>
+				<div
+					id="yellowlight"
+					className={
+						color == "yellow" ? "yellowlight active" : "yellowlight"
+					}
+					onMouseOver={e => setColor("yellow")}></div>
+				<div
+					id="greenlight"
+					className={
+						color == "green" ? "greenlight active" : "greenlight"
+					}
+					onMouseOver={e => setColor("green")}></div>
+			</div>
 		</div>
 	);
 }
